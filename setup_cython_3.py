02813e5785f7f056c65b9b757497a597684fa321
Naman Bhalla <namanbhalla1998@gmail.com>
2018-06-19 14:43:48 +0530

    Fix for Cython: “fatal error: numpy/arrayobject.h: No such file or directory”
    Source: https://stackoverflow.com/questions/14657375/cython-fatal-error-numpy-arrayobject-h-no-such-file-or-directory

--- setup_cython_3.py
+++ setup_cython_3.py
@@ -7,9 +7,9 @@ This script is used for creating .so file, which will be used by the cython  cod
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-
+import numpy
 
 #setup(extensions =[Extension('lstm_layer_cy',['lstm_layer_cy.pyx'],extra_compile_args=["-g"],extra_link_args=["-g"])], ext_modules = cythonize(Extension('lstm_layer_cy',['lstm_layer_cy.pyx'],extra_compile_args=["-g"],extra_link_args=["-g"]),gdb_debug=True )
-setup(extensions =[Extension('cython_mul_check_3',['cython_mul_check_3.pyx'],extra_compile_args=["-g"],extra_link_args=["-g"])], ext_modules = cythonize(Extension('cython_mul_check_3',['cython_mul_check_3.pyx'],extra_compile_args=["-g"],extra_link_args=["-g"]),gdb_debug=True )
+setup(extensions =[Extension('cython_mul_check_3',['cython_mul_check_3.pyx'],extra_compile_args=["-g"],extra_link_args=["-g"])], ext_modules = cythonize(Extension('cython_mul_check_3',['cython_mul_check_3.pyx'],extra_compile_args=["-g"],extra_link_args=["-g"],include_dirs=[numpy.get_include()]),gdb_debug=True )
 
       )
